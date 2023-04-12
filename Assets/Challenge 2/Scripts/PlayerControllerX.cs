58059168a4172b28942cf2ef9f555db6daa834b3
Salomão Gouveia Santos <55797110+SalomaoGouveiaSantos@users.noreply.github.com>
2023-04-12 13:51:02 -0300

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, with a delay of 1 second between the instatietions 
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1;
+        } else if(cooldown >= 0) 
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Challenge-2
+ This is the second of six challenges of the Unity plataform course
